ed66ec51367079e8200c29f80c74dcce06a59a00
Gideão Silva <gideaoms@gmail.com>
2020-07-28 10:21:53 -0300

    add validate method to request

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
-export { default as validate } from './validate'
-export { default } from './inquirer'
+import { Context, Next } from 'koa'
+import { ValidationError, ObjectSchema } from '@hapi/joi'
+
+declare module 'koa' {
+  interface Request {
+    validate: <T>(schema: ObjectSchema<any>, data?: any) => T
+  }
+}
+
+const inquirer = async ({ request, response }: Context, next: Next) => {
+  try {
+    request.validate = (schema, data) => {
+      const { error, value } = schema.validate(data ?? request.body, { abortEarly: false })
+
+      if (error) {
+        throw error
+      }
+
+      return value
+    }
+
+    await next()
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      response.status = 400
+      response.body = {
+        messages: error.details
+      }
+    } else {
+      throw error
+    }
+  }
+}
+
+export default () => inquirer
